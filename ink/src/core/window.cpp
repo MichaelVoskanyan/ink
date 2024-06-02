cccdd2aa6eadec756cf5d4670d2e7413de25e6d6
Michael Voskanyan <michael.voskanyan@icloud.com>
2024-06-02 15:13:46 -0700

    gitignore being weird?

--- ink/src/core/window.cpp
+++ ink/src/core/window.cpp
@@ -6,36 +6,37 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *wind, int width, int height) {
-    glViewport(0, 0, width, height);
+  glViewport(0, 0, width, height);
 }
 
 void Window::InitWindow() {
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
-        exit(EXIT_FAILURE);
-    }
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW\n";
+    exit(EXIT_FAILURE);
+  }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    h_window = glfwCreateWindow(i_windowWidth, i_windowHeight, s_windowTitle, NULL, NULL);
+  h_window = glfwCreateWindow(i_windowWidth, i_windowHeight, s_windowTitle,
+                              NULL, NULL);
 
-    if (!h_window) {
-        std::cerr << "Failed to create GLFW window\n";
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+  if (!h_window) {
+    std::cerr << "Failed to create GLFW window\n";
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
 
-    glfwMakeContextCurrent(h_window);
+  glfwMakeContextCurrent(h_window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize GLAD\n";
-        exit(EXIT_FAILURE);
-    }
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD\n";
+    exit(EXIT_FAILURE);
+  }
 
-    glfwSetFramebufferSizeCallback(h_window, framebuffer_size_callback);
+  glfwSetFramebufferSizeCallback(h_window, framebuffer_size_callback);
 
-    glViewport(0, 0, i_windowWidth, i_windowHeight);
-    glClearColor(.5f, .25f, 0.f, 1.f);
+  glViewport(0, 0, i_windowWidth, i_windowHeight);
+  glClearColor(.5f, .25f, 0.f, 1.f);
 }
